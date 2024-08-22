f56fba6237e33ada77d3128d1c221a359c8d1fec
Przemyslaw Smyczynski <przemyslaw.smyczynski@rst.com.pl>
2024-08-22 09:09:06 +0200

    fix mysql eventstore

--- src/mysql/Eventstore.ts
+++ src/mysql/Eventstore.ts
@@ -96,7 +96,7 @@ export class Eventstore extends EventEmitter {
       const [rows] = await connection.execute<RowDataPacket[]>(`
         SELECT event, position
           FROM ${this.namespace}_events
-          WHERE aggregateId = UuidToBin(?)
+          WHERE aggregateId = UUID_TO_BIN(?)
           ORDER BY revision DESC
           LIMIT 1
         `, [aggregateId]);
@@ -134,7 +134,7 @@ export class Eventstore extends EventEmitter {
       `
       SELECT event, position, hasBeenPublished
         FROM ${this.namespace}_events
-        WHERE aggregateId = UuidToBin(?)
+        WHERE aggregateId = UUID_TO_BIN(?)
           AND revision >= ?
           AND revision <= ?
         ORDER BY revision`,
@@ -229,7 +229,7 @@ export class Eventstore extends EventEmitter {
     for (let i = 0; i < events.length; i++) {
       const event = events[i];
 
-      placeholders.push('(UuidToBin(?), ?, ?, ?)');
+      placeholders.push('(UUID_TO_BIN(?), ?, ?, ?)');
       values.push(event.aggregate.id, event.metadata.revision, JSON.stringify(event), event.metadata.published);
     }
 
@@ -284,7 +284,7 @@ export class Eventstore extends EventEmitter {
       await connection.execute(`
         UPDATE ${this.namespace}_events
           SET hasBeenPublished = true
-          WHERE aggregateId = UuidToBin(?)
+          WHERE aggregateId = UUID_TO_BIN(?)
             AND revision >= ?
             AND revision <= ?
       `, [aggregateId, fromRevision, toRevision]);
@@ -304,7 +304,7 @@ export class Eventstore extends EventEmitter {
       const [rows] = await connection.execute<RowDataPacket[]>(`
         SELECT state, revision
           FROM ${this.namespace}_snapshots
-          WHERE aggregateId = UuidToBin(?)
+          WHERE aggregateId = UUID_TO_BIN(?)
           ORDER BY revision DESC
           LIMIT 1
       `, [aggregateId]);
@@ -340,7 +340,7 @@ export class Eventstore extends EventEmitter {
       await connection.execute(`
         INSERT IGNORE INTO ${this.namespace}_snapshots
           (aggregateId, revision, state)
-          VALUES (UuidToBin(?), ?, ?);
+          VALUES (UUID_TO_BIN(?), ?, ?);
       `, [aggregateId, revision, JSON.stringify(state)]);
     } finally {
       connection.release();
