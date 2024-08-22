e87becbb1b605091e40dce9f10ba61ed550e1f39
Przemyslaw Smyczynski <przemyslaw.smyczynski@rst.com.pl>
2024-08-22 08:00:25 +0200

    add mysql

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Eventstore as InMemoryEventstore } from './inmemory';
 export { Eventstore as MariaDbEventstore } from './mariadb';
+export { Eventstore as MysqlEventstore } from './mysql';
 export { Event } from 'commands-events';
